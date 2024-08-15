7040d20ff0642ad00fcd9d8aa7c794a31d4b8ae5
GiaKhanh-LVC <giakhanhcafetung@gmail.com>
2024-08-15 15:00:15 +0700

    Sencond commit

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Gia-Khanh-Snake-Game")
 starting_position={(-40,0),(-20,0),(0,0)}
+run=[]
 for i in starting_position:
     body=Turtle("square")
     body.color("white")
+    body.tracer(8,25)
+    body.penup()
     body.goto(i)
-    
+    run.append(body)
+while True:
+    for i in run:
+        i.update()
+        i.tracer(8,25)    
+        i.penup()
+        i.forward(20)
+        
 screen.exitonclick()
