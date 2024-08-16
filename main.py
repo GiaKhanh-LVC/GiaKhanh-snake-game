e79b0da7ca69e1ed43cf2c311ed094f1ad5e0f8c
GiaKhanh-LVC <giakhanhcafetung@gmail.com>
2024-08-16 10:02:30 +0700

    create snake

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
 import turtle
 from turtle import Turtle
 import time
-screen=turtle.Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("Gia-Khanh-Snake-Game")
-starting_position={(0,0),(-20,0),(-40,0)}
-run=[]
-screen.tracer(0)
-for i in starting_position:
-    body=Turtle("square")
-    body.color("white")
-    body.penup()
-    body.goto(i)
-    run.append(body)
-screen.update()
-while True:
-    screen.update()
-    time.sleep(0.1)
-    for i in range(2,0,-1):
-        new_x=run[i-1].xcor()
-        new_y=run[i-1].ycor()
-        run[i].goto(new_x,new_y)
-    run[0].forward(20)
-
+from snake import Snake
+new_snake=Snake()
 
 exitonclick()
